1016c62ff5d3c9202e82cf25c82d359d7810c72e
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-12-16 17:18:33 +0000

    Import from package outside current package :wink:

--- newmod/greet.py
+++ newmod/greet.py
@@ -1,5 +1,7 @@
 from submod.common import ping
+from person import Person
 
 
 def say_hi():
+    p = Person("Fayi")
     return "Hi"
---
+++ person.py
@@ -0,0 +1,6 @@
+class Person(object):
+    """docstring for Person."""
+    def __init__(self, name):
+        super(Person, self).__init__()
+        self.name = name
+        self.age = 22
